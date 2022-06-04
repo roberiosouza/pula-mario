2018681aed0aabcfa04c18106cabb1e5e99b1b57
roberio.dev <rroberiosouza@gmail.com>
2022-06-04 00:26:46 -0300

    Concluido o projeto
    Concluído as animações do jogo. 
    Mario pula;
    Muda o personagem quando perde;
    Para o túnel;
    Foi adicionado núvens;
    Estilizado o ambiente do jogo;

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function getElements(){
     var mario = document.querySelector('#mario');
+    var pipe = document.querySelector('#pipe');
+    var clouds = document.querySelector('#clouds');
 }
 
 const jump = () => {
@@ -12,5 +14,29 @@ const jump = () => {
    }, 500);
 
 }
+
+const loop = setInterval(() => {
+    getElements();
+    const pipePostion = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPostion = clouds.offsetLeft;
+
+    if (pipePostion <= 120 && pipePostion > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePostion}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '/images/game-over.png';
+        mario.style.width = '80px';
+        mario.style.marginLeft = '50px';
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPostion}px`;
+
+        clearInterval(loop);
+    }
+}, 10);
 
 document.addEventListener('keydown', jump);
